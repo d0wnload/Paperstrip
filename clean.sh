c836aa388d2aeeaf109e57cecc6a83b1b96beeb2
ka0nashi <ein.normaler.nutzer@gmail.com>
2019-10-07 13:28:36 +0200

    Add files via upload

---
+++ clean.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+for i in *.html
+do
+  echo "$i"
+cat "$i" | grep -vF '<?xml|<!DOCTYPE|<html|html>|<script|script>|<form|form>|<link|<input|<select|select>|<option|google|style=|<head|head>|<!--INFOLINKS_OFF-->|//-->' > clean/"$i"
+done
