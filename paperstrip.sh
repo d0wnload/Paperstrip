4e6ef8106b3a6931d4c653870a032ae98fe84761
c0rrupt <28587485+ka0nashi@users.noreply.github.com>
2019-10-08 02:25:38 +0200

    Update paperstrip.sh

--- paperstrip.sh
+++ paperstrip.sh
@@ -4,5 +4,5 @@ mkdir clean
 for i in *.html
 do
   echo "$i"
-cat "$i" | grep -vF '<?xml|<!DOCTYPE|<html|html>|<script|script>|<form|form>|<link|<input|<select|select>|<option|google|style=|<head|head>|<!--INFOLINKS_OFF-->|//-->' > clean/"$i"
+cat "$i" | grep -vF '<?xml|<!DOCTYPE|<html|html>|<script|script>|<form|form>|<link|<input|<select|select>|<option|google|<head|head>|<!--INFOLINKS_OFF-->|//-->' > clean/"$i"
 done
